1ed3cc8deafb0b8ccde8d18655314d9cf4e7c9f5
stangoryan <83424771+stangoryan@users.noreply.github.com>
2024-08-15 09:15:08 -0600

    Add files via upload

---
+++ infernal_bored/core/settings.lua
@@ -0,0 +1,23 @@
+local gui = require "gui"
+local settings = {
+    enabled = false,
+    elites_only = false,
+    pit_level = 1,
+    loot_enabled = true, -- Default to true
+    path_angle = 10,
+    reset_time = 1, -- Default to 1
+    selected_chest_type = nil, -- Default to material chest
+    always_open_ga_chest = false,
+}
+
+function settings:update_settings()
+    settings.enabled = gui.elements.main_toggle:get()
+    settings.elites_only = gui.elements.elite_only_toggle:get()
+    settings.loot_enabled = gui.elements.loot_toggle:get()
+    settings.loot_modes = gui.elements.loot_modes:get()
+    settings.path_angle = gui.elements.path_angle_slider:get()
+    settings.selected_chest_type = gui.elements.chest_type_selector:get()
+    settings.always_open_ga_chest = gui.elements.always_open_ga_chest:get()
+end
+
+return settings
