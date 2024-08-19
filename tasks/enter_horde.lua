6ebf021b407725bd0d4d3800f4690e258cb37edf
Holchan <holchangomes@hotmail.com>
2024-08-19 00:35:27 -0300

    first_release

--- tasks/enter_horde.lua
+++ tasks/enter_horde.lua
@@ -6,10 +6,6 @@ local explorer = require "core.explorer"
 local function enter_horde()
     local portal = utils.get_horde_portal()
     if portal then
-        console.print("Check")
-        explorer:clear_path_and_target()
-        explorer:set_custom_target(portal:get_position())
-        explorer:move_to_target()
         if utils.distance_to(portal) < 2 then
             console.print("Player is close enough to the portal. Interacting with the portal.")
             interact_object(portal)
