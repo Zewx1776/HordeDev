714b608884c4b6aa51faec30daa7937af230d394
Zewx <83424771+Zewx1776@users.noreply.github.com>
2024-08-21 18:46:41 -0600

    Add files via upload

--- tasks/exit_horde.lua
+++ tasks/exit_horde.lua
@@ -3,17 +3,34 @@ local settings = require "core.settings"
 local enums = require "data.enums"
 local tracker = require "core.tracker"
 
-local exit_horde_task = {
+exit_horde_task = {
     name = "Exit Horde",
+    delay_start_time = nil,
+    
     shouldExecute = function()
-        return utils.player_in_zone("S05_BSK_Prototype02") and utils.player_on_quest(2023962) and tracker.gold_chest_opened == true
+        return utils.player_in_zone("S05_BSK_Prototype02")
+            and utils.player_on_quest(2023962)
+            and tracker.finished_chest_looting
+            and tracker.gold_chest_successfully_opened
     end,
     
     Execute = function()
         local current_time = get_time_since_inject()
 
+        if not exit_horde_task.delay_start_time then
+            exit_horde_task.delay_start_time = current_time
+            console.print("Starting 10-second delay before initiating exit procedure")
+            return
+        end
+
+        local delay_elapsed_time = current_time - exit_horde_task.delay_start_time
+        if delay_elapsed_time < 10 then
+            console.print(string.format("Waiting to start exit procedure. Time remaining: %.2f seconds", 10 - delay_elapsed_time))
+            return
+        end
+
         if not tracker.exit_horde_start_time then
-            tracker.gold_chest_opened = true
+            tracker.finished_chest_looting = true
             console.print("Starting 10-second timer before exiting Horde")
             tracker.exit_horde_start_time = current_time
         end
@@ -23,10 +40,14 @@ local exit_horde_task = {
             console.print("10-second timer completed. Resetting all dungeons")
             reset_all_dungeons()
             tracker.exit_horde_start_time = nil
-            tracker.exit_horde_completion_time = current_time  -- Neue Zeile
+            tracker.exit_horde_completion_time = current_time
             tracker.horde_opened = false
+            tracker.finished_chest_looting = false
             tracker.gold_chest_opened = false
             tracker.start_dungeon_time = nil
+            tracker.gold_chest_successfully_opened = false
+            wave_start_time = 0
+            exit_horde_task.delay_start_time = nil  -- Reset the delay timer
         else
             console.print(string.format("Waiting to exit Horde. Time remaining: %.2f seconds", 10 - elapsed_time))
         end
