ab381cdb8d67e9ed82d97eac8c6873bfa843f5fb
Holchan <holchangomes@hotmail.com>
2024-08-24 19:18:44 -0300

    commit to work alongside greptile

--- tasks/town_salvage.lua
+++ tasks/town_salvage.lua
@@ -2,118 +2,142 @@ local utils = require "core.utils"
 local enums = require "data.enums"
 local explorer = require "core.explorer"
 local settings = require "core.settings"
-local gui = require "gui"
 local tracker = require "core.tracker"
+local gui = require "gui"
 
-local waypoints_enum = {
-    CERRIGAR = 0x76D58,
+local salvage_state = {
+    INIT = "INIT",
+    TELEPORTING = "TELEPORTING",
+    MOVING_TO_BLACKSMITH = "MOVING_TO_BLACKSMITH",
+    INTERACTING_WITH_BLACKSMITH = "INTERACTING_WITH_BLACKSMITH",
+    SALVAGING = "SALVAGING",
+    MOVING_TO_PORTAL = "MOVING_TO_PORTAL",
+    INTERACTING_WITH_PORTAL = "INTERACTING_WITH_PORTAL",
+    FINISHED = "FINISHED",
 }
 
-local task = {
+local town_salvage_task = {
     name = "Town Salvage",
+    current_state = salvage_state.INIT,
     interact_time = 0,
-    reset_salvage_time = 0, -- Add this variable to track the reset time
     portal_interact_time = 0,
-    needs_salvage = false,
 
     shouldExecute = function()
-        local should_salvage = (get_local_player():get_item_count() >= 25 and settings.loot_modes == gui.loot_modes_enum.SALVAGE)
-            or tracker.has_salvaged
-        
-        if should_salvage then
-            tracker.needs_salvage = true
-            task.needs_salvage = true
-        end
-        
-        return should_salvage
+        return (get_local_player():get_item_count() >= 25 and settings.loot_modes == gui.loot_modes_enum.SALVAGE)
     end,
+
     Execute = function(self)
         local current_time = get_time_since_inject()
+        console.print("Current state: " .. self.current_state)
 
-        local success, error = pcall(function()
-            if not utils.player_in_zone("Scos_Cerrigar") and get_local_player():get_item_count() >= 1 then
-                explorer:clear_path_and_target()
-                teleport_to_waypoint(waypoints_enum.CERRIGAR)
-                return true  -- Exit the function and wait for next tick
-            end
-
-            -- From this point on, we're guaranteed to be in Cerrigar
-            if not tracker.has_salvaged then
-                local blacksmith = utils.get_blacksmith()
-                if blacksmith then
-                    console.print("Setting target to BLACKSMITH: " .. blacksmith:get_skin_name())
-                    explorer:set_custom_target(blacksmith:get_position())  -- Pass the position, not the object
-                    explorer:move_to_target()
-
-                    if utils.distance_to(blacksmith) < 2 then
-                        if self.interact_time == 0 then
-                            console.print("Starting interaction timer.")
-                            self.interact_time = current_time
-                            return true
-                        elseif current_time - self.interact_time >= 1 and current_time - self.interact_time < 5 then
-                            if current_time - self.interact_time < 2 then  -- Only interact once
-                                console.print("Player is close enough to the blacksmith. Interacting with the blacksmith.")
-                                interact_vendor(blacksmith)
-                            end
-                            console.print(string.format("Waiting... Time elapsed: %.2f seconds", current_time - self.interact_time))
-                            return true
-                        elseif current_time - self.interact_time >= 5 then
-                            console.print("5 seconds have passed. Salvaging items.")
-                            loot_manager.salvage_all_items()
-                            tracker.has_salvaged = true
-                            self.interact_time = 0
-                            self.reset_salvage_time = current_time  -- Start the reset timer
-                        end
-                    end
-                else
-                    console.print("No blacksmith found")
-                    explorer:set_custom_target(enums.positions.blacksmith_position)
-                    explorer:move_to_target()
-                end
-            else
-                console.print("Returning to portal (currently set to blacksmith position)")
-                explorer:set_custom_target(enums.positions.portal_position)
-                explorer:move_to_target()
-
-                if enums.positions.portal_position and utils.distance_to(enums.positions.portal_position) < 5 then
-                    local portal = utils.get_town_portal()
-                    if portal then
-                        if self.portal_interact_time == 0 then
-                            console.print("Starting portal interaction timer.")
-                            self.portal_interact_time = current_time
-                            return true
-                        elseif current_time - self.portal_interact_time >= 1 and current_time - self.portal_interact_time < 5 then
-                            if current_time - self.portal_interact_time < 2 then  -- Only interact once
-                                console.print("Interacting with the portal.")
-                                interact_object(portal)
-                                tracker.has_salvaged = false
-                            end
-                            console.print(string.format("Waiting at portal... Time elapsed: %.2f seconds", current_time - self.portal_interact_time))
-                            return true
-                        elseif current_time - self.portal_interact_time >= 2 then
-                            console.print("5 seconds have passed since portal interaction.")
-                            --tracker.has_salvaged = false
-                            self.portal_interact_time = 0
-                            console.print(string.format("Time passed since salvage: %.2f seconds", current_time - self.reset_salvage_time))
-                        end
-                    else
-                        console.print("Town portal not found")
-                    end
-                end
-            end
-        end)
-
-        if not success then
-            console.print("An error occurred: " .. tostring(error))
-        else
-            if self.needs_salvage and not tracker.has_salvaged then
-                tracker.needs_salvage = false
-                self.needs_salvage = false
-            end
+        if self.current_state == salvage_state.INIT then
+            self:init_salvage()
+        elseif self.current_state == salvage_state.TELEPORTING then
+            self:teleport_to_town()
+        elseif self.current_state == salvage_state.MOVING_TO_BLACKSMITH then
+            self:move_to_blacksmith()
+        elseif self.current_state == salvage_state.INTERACTING_WITH_BLACKSMITH then
+            self:interact_with_blacksmith(current_time)
+        elseif self.current_state == salvage_state.SALVAGING then
+            self:salvage_items()
+        elseif self.current_state == salvage_state.MOVING_TO_PORTAL then
+            self:move_to_portal()
+        elseif self.current_state == salvage_state.INTERACTING_WITH_PORTAL then
+            self:interact_with_portal(current_time)
+        elseif self.current_state == salvage_state.FINISHED then
+            self:finish_salvage()
         end
-    
-        return true
-    end
+    end,
+
+    init_salvage = function(self)
+        if not utils.player_in_zone("Scos_Cerrigar") and get_local_player():get_item_count() >= 1 then
+            self.current_state = salvage_state.TELEPORTING
+        else
+            self.current_state = salvage_state.MOVING_TO_BLACKSMITH
+        end
+    end,
+
+    teleport_to_town = function(self)
+        explorer:clear_path_and_target()
+        teleport_to_waypoint(waypoints_enum.CERRIGAR)
+        self.current_state = salvage_state.MOVING_TO_BLACKSMITH
+    end,
+
+    move_to_blacksmith = function(self)
+        local blacksmith = utils.get_blacksmith()
+        if blacksmith then
+            explorer:set_custom_target(blacksmith:get_position())
+            explorer:move_to_target()
+            if utils.distance_to(blacksmith) < 2 then
+                self.current_state = salvage_state.INTERACTING_WITH_BLACKSMITH
+                self.interact_time = 0
+            end
+        else
+            console.print("No blacksmith found")
+            explorer:set_custom_target(enums.positions.blacksmith_position)
+            explorer:move_to_target()
+        end
+    end,
+
+    interact_with_blacksmith = function(self, current_time)
+        local blacksmith = utils.get_blacksmith()
+        if blacksmith then
+            if self.interact_time == 0 then
+                self.interact_time = current_time
+                interact_vendor(blacksmith)
+            elseif current_time - self.interact_time >= 5 then
+                self.current_state = salvage_state.SALVAGING
+            end
+        else
+            self.current_state = salvage_state.MOVING_TO_BLACKSMITH
+        end
+    end,
+
+    salvage_items = function(self)
+        loot_manager.salvage_all_items()
+        tracker.has_salvaged = true
+        self.current_state = salvage_state.MOVING_TO_PORTAL
+    end,
+
+    move_to_portal = function(self)
+        explorer:set_custom_target(enums.positions.portal_position)
+        explorer:move_to_target()
+        if utils.distance_to(enums.positions.portal_position) < 5 then
+            self.current_state = salvage_state.INTERACTING_WITH_PORTAL
+            self.portal_interact_time = 0
+        end
+    end,
+
+    interact_with_portal = function(self, current_time)
+        local portal = utils.get_town_portal()
+        if portal then
+            if self.portal_interact_time == 0 then
+                self.portal_interact_time = current_time
+                interact_object(portal)
+            elseif current_time - self.portal_interact_time >= 2 then
+                self.current_state = salvage_state.FINISHED
+            end
+        else
+            console.print("Town portal not found")
+            self.current_state = salvage_state.MOVING_TO_PORTAL
+        end
+    end,
+
+    finish_salvage = function(self)
+        tracker.has_salvaged = false
+        tracker.needs_salvage = false
+        self.current_state = salvage_state.INIT
+        console.print("Town salvage task finished")
+    end,
+
+    reset = function(self)
+        self.current_state = salvage_state.INIT
+        self.interact_time = 0
+        self.portal_interact_time = 0
+        tracker.has_salvaged = false
+        tracker.needs_salvage = false
+        console.print("Reset town_salvage_task and related tracker flags")
+    end,
 }
 
-return task
+return town_salvage_task
