6611998f7c7a17d1887fe4be7b0dd36e665128e0
Holchan <holchangomes@hotmail.com>
2024-08-24 21:40:51 -0300

    up until salvaging done! Working on returning back to dungeon and afterwards

--- tasks/town_salvage.lua
+++ tasks/town_salvage.lua
@@ -19,18 +19,17 @@ local salvage_state = {
 local town_salvage_task = {
     name = "Town Salvage",
     current_state = salvage_state.INIT,
-    interact_time = 0,
-    portal_interact_time = 0,
-    teleport_start_time = 0,
     max_retries = 5,
     current_retries = 0,
+    max_teleport_attempts = 5,
+    teleport_wait_time = 20,
 
     shouldExecute = function()
         return (get_local_player():get_item_count() >= 25 and settings.loot_modes == gui.loot_modes_enum.SALVAGE)
     end,
 
     Execute = function(self)
-        local current_time = get_time_since_inject()
+        console.print("Executing Town Salvage Task")
         console.print("Current state: " .. self.current_state)
 
         if self.current_retries >= self.max_retries then
@@ -42,40 +41,54 @@ local town_salvage_task = {
         if self.current_state == salvage_state.INIT then
             self:init_salvage()
         elseif self.current_state == salvage_state.TELEPORTING then
-            self:handle_teleporting(current_time)
+            self:handle_teleporting()
         elseif self.current_state == salvage_state.MOVING_TO_BLACKSMITH then
             self:move_to_blacksmith()
         elseif self.current_state == salvage_state.INTERACTING_WITH_BLACKSMITH then
-            self:interact_with_blacksmith(current_time)
+            self:interact_with_blacksmith()
         elseif self.current_state == salvage_state.SALVAGING then
             self:salvage_items()
         elseif self.current_state == salvage_state.MOVING_TO_PORTAL then
             self:move_to_portal()
         elseif self.current_state == salvage_state.INTERACTING_WITH_PORTAL then
-            self:interact_with_portal(current_time)
+            self:interact_with_portal()
         elseif self.current_state == salvage_state.FINISHED then
             self:finish_salvage()
         end
     end,
 
     init_salvage = function(self)
+        console.print("Initializing salvage process")
         if not utils.player_in_zone("Scos_Cerrigar") and get_local_player():get_item_count() >= 1 then
             self.current_state = salvage_state.TELEPORTING
             self.teleport_start_time = get_time_since_inject()
+            self.teleport_attempts = 0
+            self:teleport_to_town()
+            console.print("Player not in Cerrigar, initiating teleport")
         else
             self.current_state = salvage_state.MOVING_TO_BLACKSMITH
+            console.print("Player in Cerrigar, moving to blacksmith")
         end
     end,
-
-    handle_teleporting = function(self, current_time)
+    
+    handle_teleporting = function(self)
+        local current_time = get_time_since_inject()
         if utils.player_in_zone("Scos_Cerrigar") then
             console.print("Teleport complete, moving to blacksmith")
             self.current_state = salvage_state.MOVING_TO_BLACKSMITH
+            self.teleport_attempts = 0 -- Reset attempts counter
         else
-            if current_time - self.teleport_start_time > 10 then
+            if current_time - self.teleport_start_time > self.teleport_wait_time then
                 console.print("Teleport taking too long, retrying...")
+                self.teleport_attempts = (self.teleport_attempts or 0) + 1
+                
+                if self.teleport_attempts >= self.max_teleport_attempts then
+                    console.print("Max teleport attempts reached. Resetting task.")
+                    self:reset()
+                    return
+                end
+                
                 self:teleport_to_town()
-                self.current_retries = self.current_retries + 1
             else
                 console.print("Still teleporting, waiting...")
             end
@@ -83,21 +96,25 @@ local town_salvage_task = {
     end,
 
     teleport_to_town = function(self)
+        console.print("Teleporting to town")
         explorer:clear_path_and_target()
         teleport_to_waypoint(enums.waypoints.CERRIGAR)
-        if utils.wait(5) then  -- Adjust the wait time as needed
-            self.current_state = salvage_state.MOVING_TO_BLACKSMITH
-        end
+        self.teleport_start_time = get_time_since_inject()
+        console.print("Teleport command issued")
     end,
 
     move_to_blacksmith = function(self)
+        console.print("Moving to blacksmith")
+        console.print("Explorer object: " .. tostring(explorer))
+        console.print("set_custom_target exists: " .. tostring(type(explorer.set_custom_target) == "function"))
+        console.print("move_to_target exists: " .. tostring(type(explorer.move_to_target) == "function"))
         local blacksmith = utils.get_blacksmith()
         if blacksmith then
             explorer:set_custom_target(blacksmith:get_position())
             explorer:move_to_target()
             if utils.distance_to(blacksmith) < 2 then
+                console.print("Reached blacksmith")
                 self.current_state = salvage_state.INTERACTING_WITH_BLACKSMITH
-                self.interact_time = 0
             end
         else
             console.print("No blacksmith found, retrying...")
@@ -107,38 +124,46 @@ local town_salvage_task = {
         end
     end,
 
-    interact_with_blacksmith = function(self, current_time)
+    interact_with_blacksmith = function(self)
+        console.print("Interacting with blacksmith")
         local blacksmith = utils.get_blacksmith()
         if blacksmith then
             interact_vendor(blacksmith)
-            if utils.wait(5) then  -- Use utils.wait instead of manual time checking
+            if tracker.check_time("blacksmith_interaction", 5) then
+                console.print("Interaction complete, moving to salvage")
                 self.current_state = salvage_state.SALVAGING
             end
         else
+            console.print("Blacksmith not found, moving back")
             self.current_state = salvage_state.MOVING_TO_BLACKSMITH
         end
     end,
 
     salvage_items = function(self)
+        console.print("Salvaging items")
         loot_manager.salvage_all_items()
         tracker.has_salvaged = true
+        console.print("Salvage complete, moving to portal")
         self.current_state = salvage_state.MOVING_TO_PORTAL
     end,
 
     move_to_portal = function(self)
+        console.print("Moving to portal")
         explorer:set_custom_target(enums.positions.portal_position)
         explorer:move_to_target()
         if utils.distance_to(enums.positions.portal_position) < 5 then
+            console.print("Reached portal")
             self.current_state = salvage_state.INTERACTING_WITH_PORTAL
-            self.portal_interact_time = 0
         end
     end,
 
     interact_with_portal = function(self)
+        console.print("Interacting with portal")
         local portal = utils.get_town_portal()
         if portal then
             interact_object(portal)
-            if utils.wait(2) then  -- Use utils.wait instead of manual time checking
+            if tracker.check_time("portal_interaction", 2) then
+                console.print("Portal interaction complete")
                 self.current_state = salvage_state.FINISHED
             end
         else
@@ -148,6 +173,7 @@ local town_salvage_task = {
     end,
 
     finish_salvage = function(self)
+        console.print("Finishing salvage task")
         tracker.has_salvaged = false
         tracker.needs_salvage = false
         self.current_state = salvage_state.INIT
@@ -156,6 +182,7 @@ local town_salvage_task = {
     end,
 
     reset = function(self)
+        console.print("Resetting town salvage task")
         self.current_state = salvage_state.INIT
         tracker.has_salvaged = false
         tracker.needs_salvage = false
