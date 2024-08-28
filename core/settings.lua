bfe6125a7e7b9d5d9451300df9d473ca0eccab3c
Letrico <muntat@gmail.com>
2024-08-28 13:16:00 +1000

    fix wrong variable name and float slider

--- core/settings.lua
+++ core/settings.lua
@@ -18,7 +18,7 @@ function settings:update_settings()
     settings.always_open_ga_chest = gui.elements.always_open_ga_chest:get()
     settings.loot_mothers_gift = gui.elements.loot_mothers_gift:get()
     settings.merry_go_round = gui.elements.merry_go_round:get()
-    settings.chest_open_delay = gui.elements.chest_open_delay:get()
+    settings.open_chest_delay = gui.elements.open_chest_delay:get()
     settings.boss_kill_delay = gui.elements.boss_kill_delay:get()
     settings.chest_move_attempts = gui.elements.chest_move_attempts:get()
 end
--- gui.lua
+++ gui.lua
@@ -30,7 +30,7 @@ gui.elements = {
     always_open_ga_chest = create_checkbox("always_open_ga_chest"),
     loot_mothers_gift = create_checkbox("loot_mothers_gift"),
     merry_go_round = checkbox:new(true, get_hash("merry_go_round")),
-    chest_open_delay = slider_float:new(1.0, 3.0, 1.5, get_hash("chest_open_delay")), -- 1.0 is the default value
+    open_chest_delay = slider_float:new(1.0, 3.0, 1.5, get_hash("open_chest_delay")), -- 1.0 is the default value
     boss_kill_delay = slider_int:new(1, 10, 6, get_hash("boss_kill_delay")), -- 6 is a default value
     chest_move_attempts = slider_int:new(20, 400, 40, get_hash("chest_move_attempts")), -- 20 is a default value
 }
@@ -51,7 +51,7 @@ function gui.render()
         gui.elements.always_open_ga_chest:render("Always Open GA Chest", "Toggle to always open Greater Affix chest when available")
         gui.elements.loot_mothers_gift:render("Loot Mother's Gift", "Toggle to loot Mother's Gift")
         gui.elements.merry_go_round:render("Circle arena when wave completes", "Toggle to circle arene when wave completes to pick up stray Aethers")
-        gui.elements.open_chest_delay:render("Chest open delay", "Adjust delay for the chest opening (1.0-3.0)")
+        gui.elements.open_chest_delay:render("Chest open delay", "Adjust delay for the chest opening (1.0-3.0)", 2)
         gui.elements.boss_kill_delay:render("Boss kill delay", "Adjust delay after killing boss (1-10)")
         gui.elements.chest_move_attempts:render("Chest move attempts", "Adjust the amount of times it tries to reach a chest (20-400)")
         
